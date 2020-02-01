575826429ea71c6dcf9ab3f1f462746bdc2cec74
kkyehit <rp01020113994@gmail.com>
2020-02-01 12:59:44 +0000

    hanyang 6th beginner > D

---
+++ Backjoon/17946/src.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(void){
+	int C;
+	scanf("%d", &C);
+	while(C--){
+		long long int res = 0, n;
+		scanf("%lld", &n);
+		printf("%lld\n",1);
+	}
+}
